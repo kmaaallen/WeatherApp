17998917e4d4ee1a96bec00b77b4a32037875bd2
kmaaallen@gmail.com <kmaaallen@gmail.com>
2021-06-28 07:18:11 +0100

    Add currentWeather comp to app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import CurrentWeather from './components/CurrentWeather';
 
 function App() {
   return (
     <div className="App">
-
+      <CurrentWeather />
     </div>
   );
 }
