d664676409e37cc0e56b20808aa6d19c3e533d04
kmaaallen@gmail.com <kmaaallen@gmail.com>
2021-06-29 09:05:42 +0100

    Added background images and theme file to be built out

---
+++ src/utils/theme.js
@@ -0,0 +1,9 @@
+//Weather condition backgrounds
+
+// Thunderstorm - barry-simon-4C6Rp23RjnE-unsplash
+// Drizzle - barry-simon-4C6Rp23RjnE-unsplash
+// Rain - barry-simon-4C6Rp23RjnE-unsplash
+// Snow - barry-simon-4C6Rp23RjnE-unsplash
+// Atmosphere - Mist, Smoke, Haze, Dust, Fog, Sand, Ash, Squall, Tornado - barry-simon-4C6Rp23RjnE-unsplash
+// Clear - barry-simon-4C6Rp23RjnE-unsplash
+// Clouds - barry-simon-4C6Rp23RjnE-unsplash
