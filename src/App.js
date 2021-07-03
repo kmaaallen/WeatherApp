83fdfc57957fcc06f9feb78da7477dc5a50363b5
kmaaallen@gmail.com <kmaaallen@gmail.com>
2021-07-03 11:31:56 +0100

    refactor to change bg when data changes

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ import Clear from './images/Clear.jpg';
 import Thunderstorm from './images/Thunderstorm.jpg';
 import Drizzle from './images/Drizzle.jpg';
 
-
 function App() {
   const [latitude, setLatitude] = useState('');
   const [longitude, setLongitude] = useState('');
@@ -22,8 +21,23 @@ function App() {
   const firstCall = useRef(true);
 
   function myCallBack(cityData) {
-    setData(cityData)
+    setData(cityData);
   };
+
+  useLayoutEffect(() => {
+    if (data.length === 0) {
+      return;
+    } else {
+      const atmosphere = ['Mist', 'Smoke', 'Haze', 'Dust', 'Fog', 'Sand', 'Ash', 'Squall', 'Tornado'];
+      if (atmosphere.includes(data.weather[0].main)) setBackground(Atmosphere);
+      if (data.weather[0].main === 'Clouds') setBackground(Clouds);
+      if (data.weather[0].main === 'Rain') setBackground(Rain);
+      if (data.weather[0].main === 'Snow') setBackground(Snow);
+      if (data.weather[0].main === 'Drizzle') setBackground(Drizzle);
+      if (data.weather[0].main === 'Thunderstorm') setBackground(Thunderstorm);
+      if (data.weather[0].main === 'Clear') setBackground(Clear);
+    }
+  }, [data])
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(function (pos) {
@@ -33,7 +47,6 @@ function App() {
   }, []);
 
   useLayoutEffect(() => {
-    const atmosphere = ['Mist', 'Smoke', 'Haze', 'Dust', 'Fog', 'Sand', 'Ash', 'Squall', 'Tornado'];
     if (firstCall.current) {
       firstCall.current = false;
       return;
@@ -42,16 +55,6 @@ function App() {
         .then((res) => res.json())
         .then((data) => {
           setData(data.data);
-
-          if (atmosphere.includes(data.data.weather[0].main)) setBackground(Atmosphere);
-          if (data.data.weather[0].main === 'Clouds') setBackground(Clouds);
-          if (data.data.weather[0].main === 'Rain') setBackground(Rain);
-          if (data.data.weather[0].main === 'Snow') setBackground(Snow);
-          if (data.data.weather[0].main === 'Drizzle') setBackground(Drizzle);
-          if (data.data.weather[0].main === 'Thunderstorm') setBackground(Thunderstorm);
-          if (data.data.weather[0].main === 'Clear') setBackground(Clear);
-
-          console.log(data);
         });
     }
   }, [latitude, longitude]);
