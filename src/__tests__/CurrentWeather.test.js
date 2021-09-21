9b4fbcef1788a0d6bba1cb99258d53e09c5e9337
kmaaallen@gmail.com <kmaaallen@gmail.com>
2021-09-21 08:11:04 +0100

    Test: Add to render unit tests for CurrentWeather Check all info fields displaying correctly for passed data

--- src/__tests__/CurrentWeather.test.js
+++ src/__tests__/CurrentWeather.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { render } from '@testing-library/react';
 import CurrentWeather from '../components/CurrentWeather';
 import { data } from '../setupTests';
 
@@ -8,9 +9,23 @@ describe('<CurrentWeather />', () => {
     it('renders correctly when provided with valid data object', () => {
         const tree = renderer.create(<CurrentWeather data={data} />).toJSON();
         expect(tree).toMatchSnapshot();
+
+        const { getByText, getByTitle } = render(<CurrentWeather data={data} />);
+        const display = getByText('Mountain View, US');
+        const media = getByTitle('clear sky icon');
+        const temp = getByText('283°C');
+        const max = getByText('Max: 284°C');
+        const min = getByText('Min: 280°C');
+        const sunrise = getByText('Sunrise: 5:47 am');
+        const sunset = getByText('Sunset: 8:29 pm');
+        expect(display && media && temp && max && min && sunrise && sunset).toBeTruthy();
     });
     it('renders correctly when provided with empty data object', () => {
         const tree = renderer.create(<CurrentWeather data={[]} />).toJSON();
         expect(tree).toMatchSnapshot();
+
+        const { getByText } = render(<CurrentWeather data={[]} />);
+        const loading = getByText('Loading ...');
+        expect(loading).toBeTruthy();
     });
 });
