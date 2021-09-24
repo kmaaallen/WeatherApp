91e93a202e47219dc33828330e9e4ef2ac15de8f
kmaaallen@gmail.com <kmaaallen@gmail.com>
2021-09-24 08:36:24 +0100

    Test: Add unit test for night render To expect white text that comes with night class

--- src/__tests__/CurrentWeather.test.js
+++ src/__tests__/CurrentWeather.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import { render } from '@testing-library/react';
 import CurrentWeather from '../components/CurrentWeather';
-import { data } from '../setupTests';
+import { data, nightData } from '../setupTests';
 
 
 describe('<CurrentWeather />', () => {
@@ -28,4 +28,14 @@ describe('<CurrentWeather />', () => {
         const loading = getByText('Loading ...');
         expect(loading).toBeTruthy();
     });
+    it('renders white text when weather locale is night time', () => {
+        const { getByText } = render(<CurrentWeather data={nightData} />);
+        const display = getByText('Mountain View, US').classList.toString();
+        const temp = getByText('283°C').classList.toString();
+        const max = getByText('Max: 284°C').classList.toString();
+        const min = getByText('Min: 280°C').classList.toString();
+        const sunrise = getByText('Sunrise: 5:47 am').classList.toString();
+        const sunset = getByText('Sunset: 8:29 pm').classList.toString();
+        expect(display && temp && max && min && sunrise && sunset).toContain('night');
+    })
 });
